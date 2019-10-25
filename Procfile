c80d98c2ecffe3c4f6eddc7dfc4fdc39c4c44e59
雨滴 <yudi@yudideMacBook-Air.local>
2019-10-25 05:39:27 -0400

    prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ins.wsgi --log-file -
--- ins/settings.py
+++ ins/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '61ja*zl)93+=@9%f+#&2p=o-i16ocb^+)7-=s3%gb4a5vfk_13'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'insta',
     'imagekit'
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
